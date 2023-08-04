da65466c106da57cd2267a8758930b878c130d91
Rob Chambers <robch@microsoft.com>
2023-08-03 18:52:32 -0700

    better speech help

--- src/ai/commands/parsers/speech_command_parser.cs
+++ src/ai/commands/parsers/speech_command_parser.cs
@@ -26,13 +26,75 @@ namespace Azure.AI.Details.Common.CLI
             ("speech.synthesize", true),
             ("speech.intent", true),
             ("speech.translate", true),
+            ("speech.batch.list", true),
+            ("speech.batch.download", true),
+            ("speech.batch.transcription.create", true),
+            ("speech.batch.transcription.update", true),
+            ("speech.batch.transcription.delete", true),
+            ("speech.batch.transcription.status", true),
+            ("speech.batch.transcription.download", true),
+            ("speech.batch.transcription.list", false),
+            ("speech.batch.transcription.onprem.create", true),
+            ("speech.batch.transcription.onprem.status", true),
+            ("speech.batch.transcription.onprem.delete", true),
+            ("speech.batch.transcription.onprem.endpoints", true),
+            ("speech.batch.transcription.onprem.list", false),
             ("speech.batch", true),
+            ("speech.csr.list", true),
+            ("speech.csr.download", true),                          
+            ("speech.csr.project.create", true),
+            ("speech.csr.project.update", true),
+            ("speech.csr.project.delete", true),
+            ("speech.csr.project.status", true),
+            ("speech.csr.project.list", false),
+            ("speech.csr.dataset.create", true),
+            ("speech.csr.dataset.upload", true),
+            ("speech.csr.dataset.update", true),
+            ("speech.csr.dataset.delete", true),
+            ("speech.csr.dataset.list", false),
+            ("speech.csr.dataset.status", true),
+            ("speech.csr.dataset.download", true),
+            ("speech.csr.model.create", true),
+            ("speech.csr.model.update", true),
+            ("speech.csr.model.delete", true),
+            ("speech.csr.model.list", false),
+            ("speech.csr.model.status", true),
+            ("speech.csr.model.download", true),
+            ("speech.csr.model.copy", true),
+            ("speech.csr.evaluation.create", true),
+            ("speech.csr.evaluation.list", false),
+            ("speech.csr.evaluation.show", true),
+            ("speech.csr.evaluation.status", true),
+            ("speech.csr.evaluation.delete", true),
+            ("speech.csr.endpoint.create", true),
+            ("speech.csr.endpoint.update", true),
+            ("speech.csr.endpoint.delete", true),
+            ("speech.csr.endpoint.list", false),
+            ("speech.csr.endpoint.status", true),
+            ("speech.csr.endpoint.download", true),
             ("speech.csr", true),
+            ("speech.profile.list", false),
+            ("speech.profile.create", true),
+            ("speech.profile.status", true),
+            ("speech.profile.enroll", true),
+            ("speech.profile.delete", true),
             ("speech.profile", true),
-            ("speech.speaker", true),
+            ("speech.speaker.identify", true),
+            ("speech.speaker.verify", true),
+            ("speech.speaker", true)
         };
 
         private static readonly string[] _partialCommands = {
+            "speech.batch.transcription",
+            "speech.batch",
+            "speech.csr.project",
+            "speech.csr.dataset",
+            "speech.csr.model",
+            "speech.csr.evaluation",
+            "speech.csr.endpoint",
+            "speech.csr",
+            "speech.profile",
+            "speech.speaker",
             "speech"
         };
 
@@ -50,6 +112,9 @@ namespace Azure.AI.Details.Common.CLI
                 case "speech.profile": return speechProfileParsers;
                 case "speech.speaker": return speechSpeakerParsers;
             }
+
+            if (commandName.StartsWith("speech.batch")) return speechBatchParsers;
+            if (commandName.StartsWith("speech.csr")) return speechCsrParsers;
 
             return null;
         }
