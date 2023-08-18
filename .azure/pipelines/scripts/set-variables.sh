76d6b7265892beb31fadfec748931c5fb69c10ca
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-08-18 15:32:01 -0700

    fixing script

--- .azure/pipelines/scripts/set-variables.sh
+++ .azure/pipelines/scripts/set-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Source branch: $BUILD_SOURCEBRANCH"
+
 # If the build was triggered from a tag, use the tag as the version. Otherwise, set the version to dev.
 REGEX='^refs\/tags\/v?([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)(-.+)?'
-VERSION=$([[ $1 =~ $REGEX ]] && echo $(echo $1 | sed -r 's/'$REGEX'/\1.\2.\3\4/') || echo "0.1.0-dev")
+VERSION=$([[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && echo $(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/') || echo "0.1.0-dev")
 
 # Set the AICLIVersion variable in the pipeline.
 echo "##vso[task.setvariable variable=AICLIVersion]$VERSION"
