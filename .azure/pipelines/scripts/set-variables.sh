a90dc8807064e0e62343a5c0c5c90446d6095e4b
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-08-28 13:52:55 -0700

    variables

--- .azure/pipelines/scripts/set-variables.sh
+++ .azure/pipelines/scripts/set-variables.sh
@@ -7,16 +7,16 @@ REGEX='^refs\/tags\/v?([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)(-.+)?'
 
 # If tag is a release tag, set up release variables.
 if [[ $BUILD_SOURCEBRANCH =~ $REGEX ]]; then
-    echo "##vso[task.setvariable variable=ShouldCreateRelease]true"
+    echo "##vso[task.setvariable variable=ShouldCreateRelease; isOutput=true;]true"
 
     # Extract version from the tag.
     VERSION=$(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/')
 
     # Set the AICLIVersion variable in the pipeline.
-    echo "##vso[task.setvariable variable=AICLIVersion]$VERSION"
+    echo "##vso[task.setvariable variable=AICLIVersion; isOutput=true;]$VERSION"
 
     # Set the AICLINuPkgFileName variable in the pipeline.
-    echo "##vso[task.setvariable variable=AICLINuPkgFileName]Azure.AI.CLI.$VERSION.nupkg"
+    echo "##vso[task.setvariable variable=AICLINuPkgFileName; isOutput=true;]Azure.AI.CLI.$VERSION.nupkg"
 else
-    echo "##vso[task.setvariable variable=ShouldCreateRelease]false"
+    echo "##vso[task.setvariable variable=ShouldCreateRelease; isOutput=true;]false"
 fi
