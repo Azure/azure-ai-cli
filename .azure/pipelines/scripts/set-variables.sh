b5570e4707d4ba151867d8c4fe1e4adb4506b179
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-08-28 13:41:14 -0700

    Reestructuring pipeline definitions to future-proof it.

--- .azure/pipelines/scripts/set-variables.sh
+++ .azure/pipelines/scripts/set-variables.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+define_variable () {
+    echo "##vso[task.setvariable variable=$1;isOutput=true]$2"
+}
+
 echo "Source branch: $BUILD_SOURCEBRANCH"
 
 # If the build was triggered from a tag, use the tag as the version. Otherwise, set the version to dev.
 REGEX='^refs\/tags\/v?([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)(-.+)?'
 
 # If tag is a release tag, set up release variables.
-if [[ $BUILD_SOURCEBRANCH =~ $REGEX ]]; then
-    echo "##vso[task.setvariable variable=ShouldCreateRelease]true"
+[[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && define_variable "IsRelease" "true" || define_variable "IsRelease" "false"
 
-    # Extract version from the tag.
-    VERSION=$(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/')
+# Extract version from the tag.
+VERSION=$([[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && echo $(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/') || echo "0.0.0-dev")
 
-    # Set the AICLIVersion variable in the pipeline.
-    echo "##vso[task.setvariable variable=AICLIVersion]$VERSION"
+# Set the AICLIVersion variable in the pipeline.
+define_variable "AICLIVersion" "$VERSION"
 
-    # Set the AICLINuPkgFileName variable in the pipeline.
-    echo "##vso[task.setvariable variable=AICLINuPkgFileName]Azure.AI.CLI.$VERSION.nupkg"
-else
-    echo "##vso[task.setvariable variable=ShouldCreateRelease]false"
-fi
+# Set the AICLINuPkgFileName variable in the pipeline.
+define_variable "AICLINuPkgFileName" "Azure.AI.CLI.$VERSION.nupkg"
