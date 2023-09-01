8c7d65216ac4ac0e8531db1c1ec2f64f666e202c
Rob Chambers <robc@thinkman.com>
2023-09-01 02:09:50 +0000

    getting closer; logged bug on Python SDK for blocking issue (#2662868)

--- src/common/details/commands/init_command.cs
+++ src/common/details/commands/init_command.cs
@@ -197,12 +197,12 @@ namespace Azure.AI.Details.Common.CLI
             var id = resource.Id;
 
             ConsoleHelpers.WriteLineWithHighlight($"\n`OPEN AI DEPLOYMENT (CHAT)`");
-            var deployment = await AzCliConsoleGui.AiResourceDeploymentPicker.PickOrCreateDeployment(interactive, "Chat", subscriptionId, resource, null);
+            var deployment = await AzCliConsoleGui.AiResourceDeploymentPicker.PickOrCreateDeployment(interactive, "Chat", subscriptionId, resource.Group, resource.RegionLocation, resource.Name, null);
             var chatDeploymentName = deployment.Name;
 
             ConsoleHelpers.WriteLineWithHighlight($"\n`OPEN AI DEPLOYMENT (EMBEDDINGS)`");
 
-            var embeddingsDeployment = await AzCliConsoleGui.AiResourceDeploymentPicker.PickOrCreateDeployment(interactive, "Embeddings", subscriptionId, resource, null);
+            var embeddingsDeployment = await AzCliConsoleGui.AiResourceDeploymentPicker.PickOrCreateDeployment(interactive, "Embeddings", subscriptionId, resource.Group, resource.RegionLocation, resource.Name, null);
             var embeddingsDeploymentName = embeddingsDeployment.Name;
 
             var keys = await AzCliConsoleGui.LoadCognitiveServicesResourceKeys(subscriptionId, resource);
