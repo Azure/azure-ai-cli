d51a6fb6d1b9b1b3b6940c75ef21065580a9f108
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-10-06 10:54:43 -0700

    Changing the way we load the native speech library. (#39)

--- src/common/details/native_logging/SpeechNativeMethods.cs
+++ src/common/details/native_logging/SpeechNativeMethods.cs
@@ -11,45 +11,21 @@ namespace Azure.AI.Details.Common.CLI
     /// </summary>
     internal static class SpeechNativeMethods
     {
-        public const string IOS_NativeDllName = "__Internal";
-        public const CallingConvention IOS_NativeCallConvention = CallingConvention.Cdecl;
-
-        public const string OSX_NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.dylib";
-        public const CallingConvention OSX_NativeCallConvention = CallingConvention.Cdecl;
-
-        public const string UNIX_NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.so";
-        public const CallingConvention UNIX_NativeCallConvention = CallingConvention.Cdecl;
-
-        public const string Android_NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.so";
-        public const CallingConvention Android_NativeCallConvention = CallingConvention.Cdecl;
-
-        public const string Windows_NativeDllName = "Microsoft.CognitiveServices.Speech.core.dll";
-        public const CallingConvention Windows_NativeCallConvention = CallingConvention.StdCall;
+        private const string NativeDllName = "Microsoft.CognitiveServices.Speech.core";
 
         #if IOS
-            public const string NativeDllName = "__Internal";
-            public const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
+            private const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
         #elif OSX
-            public const string NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.dylib";
-            public const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
+            private const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
         #elif UNIX
-            public const string NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.so";
-            public const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
+            private const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
         #elif Android
-            public const string NativeDllName = "libMicrosoft.CognitiveServices.Speech.core.so";
-            public const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
+            private const CallingConvention NativeCallConvention = CallingConvention.Cdecl;
         #else
-            public const string NativeDllName = "Microsoft.CognitiveServices.Speech.core.dll";
-            public const CallingConvention NativeCallConvention = CallingConvention.StdCall;
+            private const CallingConvention NativeCallConvention = CallingConvention.StdCall;
         #endif
 
         [DllImport(NativeDllName, CallingConvention = NativeCallConvention)]
-        public static extern void diagnostics_log_trace_string(int level, IntPtr title, IntPtr fileName, int lineNumber, IntPtr message);
-
-        [DllImport(Android_NativeDllName, CallingConvention = Android_NativeCallConvention, EntryPoint = "diagnostics_log_trace_string")]
-        public static extern void android_diagnostics_log_trace_string(int level, IntPtr title, IntPtr fileName, int lineNumber, IntPtr message);
-
-        [DllImport(UNIX_NativeDllName, CallingConvention = UNIX_NativeCallConvention, EntryPoint = "diagnostics_log_trace_string")]
-        public static extern void unix_diagnostics_log_trace_string(int level, IntPtr title, IntPtr fileName, int lineNumber, IntPtr message);
+        internal static extern void diagnostics_log_trace_string(int level, IntPtr title, IntPtr fileName, int lineNumber, IntPtr message);
     }
 }
