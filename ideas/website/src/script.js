106a81514e0928baccc6e9a24e2e0fcf14da03f0
Rob Chambers <robc@thinkman.com>
2023-12-21 08:55:46 -0800

    refactor to increase readability

--- ideas/website/src/script.js
+++ ideas/website/src/script.js
@@ -1,40 +1,44 @@
-const chatCompletions = require('./ChatCompletionsStreaming');
 const marked = require("marked");
 const hljs = require("highlight.js");
 
-function newChat() {
-  let chatPanel = document.getElementById("chatPanel");
-  chatPanel.innerHTML = '';
-  let logo = document.getElementById("logo");
-  logo.style.display = "block";
-  document.getElementById("userInput").focus();
+const chatCompletions = require('./ChatCompletionsStreaming');
+const streamingChatIncompleteEnding = '◼️';
+let streamingChatCompletions;
+
+function streamingChatCompletionsInit() {
+  const endpoint = process.env.OPENAI_ENDPOINT;
+  const azureApiKey = process.env.OPENAI_API_KEY;
+  const deploymentName = process.env.AZURE_OPENAI_CHAT_DEPLOYMENT;
+  const systemPrompt = "You are a helpful AI assistant.";
+
+  if (!endpoint) {
+    chatPanelAppendMessage('computer', 'Please set OPENAI_ENDPOINT in .env');
+  }
+  if (!azureApiKey) {
+    chatPanelAppendMessage('computer', 'Please set OPENAI_API_KEY in .env');
+  }
+  if (!deploymentName) {
+    chatPanelAppendMessage('computer', 'Please set AZURE_OPENAI_CHAT_DEPLOYMENT in .env');
+  }
+
+  streamingChatCompletions = new chatCompletions.StreamingChatCompletionsHelper(systemPrompt, endpoint, azureApiKey, deploymentName);
+}
+
+function streamingChatCompletionsClear() {
   streamingChatCompletions.clearConversation();
 }
 
-function sendMessage() {
-  let userInput = document.getElementById("userInput");
-  let userInputValue = userInput.value;
-  if (userInputValue.trim() !== '') {
-    appendMessage('user', markdownToHtml(userInputValue));
-    userInput.value = '';
-    updateUserInputHeight();
-    updateWidthsAndHeights();
-    getChatCompletions(userInputValue);
-  }
-}
+async function streamingChatCompletionsProcessInput(userInput) {
+  let newMessage = chatPanelAppendMessage('computer', streamingChatIncompleteEnding);
 
-async function getChatCompletions(userInput) {
-  let ending = '◼️';
-  let newMessage = appendMessage('computer', ending);
   let computerResponse = await streamingChatCompletions.getChatCompletions(userInput, function (response) {
-    let chatPanel = document.getElementById("chatPanel");
-    let atBottomNow = Math.abs(chatPanel.scrollHeight - chatPanel.clientHeight - chatPanel.scrollTop) < 1;
+    let atBottomBeforeUpdate = chatPanelIsScrollAtBottom();
 
-    let newContent = newMessage.innerHTML.replace(ending, response) + ending;
+    let newContent = newMessage.innerHTML.replace(streamingChatIncompleteEnding, response) + streamingChatIncompleteEnding;
     newMessage.innerHTML = newContent.replace(/\n/g, '<br>');
 
-    if (atBottomNow) {
-      chatPanel.scrollTop = chatPanel.scrollHeight;
+    if (atBottomBeforeUpdate) {
+      chatPanelScrollToBottom();
     }
   });
 
@@ -42,41 +46,153 @@ async function getChatCompletions(userInput) {
   chatPanel.scrollTop = chatPanel.scrollHeight;
 }
 
+function chatPanelGetElement() {
+  return document.getElementById("chatPanel");
+}
+
+function chatPanelAppendMessage(sender, message) {
+  logoHide();
+
+  let messageContent = document.createElement("p");
+  messageContent.className = "message-content";
+  messageContent.innerHTML = message;
+
+  let messageAuthor = document.createElement("p");
+  messageAuthor.className = "message-author";
+  messageAuthor.innerHTML = sender == "user" ? "You" : "Assistant";
+
+  let divContainingBoth = document.createElement("div");
+  divContainingBoth.className = sender === "user" ? "user" : "computer";
+  divContainingBoth.appendChild(messageAuthor);
+  divContainingBoth.appendChild(messageContent);
+
+  let chatPanel = chatPanelGetElement();
+  chatPanel.appendChild(divContainingBoth);
+  chatPanelScrollToBottom();
+
+  return messageContent;
+}
+
+function chatPanelIsScrollAtBottom() {
+  let chatPanel = chatPanelGetElement();
+  let atBottom = Math.abs(chatPanel.scrollHeight - chatPanel.clientHeight - chatPanel.scrollTop) < 1;
+  return atBottom;
+}
+
+function chatPanelScrollToBottom() {
+  let chatPanel = chatPanelGetElement();
+  chatPanel.scrollTop = chatPanel.scrollHeight;
+}
+
+function chatPanelClear() {
+  let chatPanel = chatPanelGetElement();
+  chatPanel.innerHTML = '';
+}
+
+function logoGetElement() {
+  return document.getElementById("logo");
+}
+
+function logoShow() {
+  let logo = logoGetElement();
+  logo.style.display = "block";
+}
+
+function logoHide() {
+  let logo = logoGetElement();
+  logo.style.display = "none";
+}
+
+function markdownInit() {
+  marked.setOptions({
+    highlight: function (code, lang) {
+      let hl = lang === undefined || lang === ''
+        ? hljs.highlightAuto(code).value
+        : hljs.highlight(lang, code).value;
+      return `<div class="hljs">${hl}</div>`;
+    }
+  });
+}
+
 function markdownToHtml(markdownText) {
   return marked.parse(markdownText);
 }
 
-function appendMessage(sender, message) {
-  let logo = document.getElementById("logo");
-  logo.style.display = "none";
-
-  let p = document.createElement("p");
-  p.className = "message-content";
-  p.innerHTML = message;
-
-  let p2 = document.createElement("p");
-  p2.className = "message-author";
-  p2.innerHTML = sender == "user" ? "You" : "Assistant";
-
-  let d = document.createElement("div");
-  d.className = sender === "user" ? "user" : "computer";
-  d.appendChild(p2);
-  d.appendChild(p);
-
-  let chatPanel = document.getElementById("chatPanel");
-  chatPanel.appendChild(d);
-  chatPanel.scrollTop = chatPanel.scrollHeight;
-
-  return p;
+function themeToggle() {
+  let iconElement = toggleThemeButtonGetElement().children[0];
+  let bodyElement = document.body;
+  if (bodyElement.classList.contains("light-theme")) {
+    bodyElement.classList.remove("light-theme");
+    iconElement.classList.remove("fa-toggle-off");
+    iconElement.classList.add("fa-toggle-on");
+  } else {
+    bodyElement.classList.add("light-theme");
+    iconElement.classList.remove("fa-toggle-on");
+    iconElement.classList.add("fa-toggle-off");
+  }
 }
 
-function updateUserInputHeight() {
-  let userInput = document.getElementById("userInput");
-  userInput.style.height = 'auto';
-  userInput.style.height = (userInput.scrollHeight) + 'px';
+function toggleThemeButtonGetElement() {
+  return document.getElementById("toggleThemeButton");
 }
 
-function updateWidthsAndHeights() {
+function toggleThemeButtonInit() {
+  let buttonElement = toggleThemeButtonGetElement();
+  buttonElement.addEventListener("click", themeToggle);
+  buttonElement.addEventListener('keydown', toggleThemeButtonHandleKeyDown());
+}
+
+function toggleThemeButtonHandleKeyDown() {
+  return function (event) {
+    if (event.code === 'Enter' || event.code === 'Space') {
+      themeToggle();
+    }
+  };
+}
+
+function userInputTextAreaGetElement() {
+  return document.getElementById("userInput");
+}
+
+function userInputTextAreaInit() {
+  let inputElement = userInputTextAreaGetElement();
+  inputElement.addEventListener("keydown", userInputTextAreaHandleKeyDown());
+  inputElement.addEventListener("input", userInputTextAreaUpdateHeight);
+}
+
+function userInputTextAreaFocus() {
+  let inputElement = userInputTextAreaGetElement();
+  inputElement.focus();
+}
+
+function userInputTextAreaClear() {
+  userInputTextAreaGetElement().value = '';
+  userInputTextAreaUpdateHeight();
+}
+
+function userInputTextAreaUpdateHeight() {
+  let inputElement = userInputTextAreaGetElement();
+  inputElement.style.height = 'auto';
+  inputElement.style.height = (userInput.scrollHeight) + 'px';
+}
+
+function userInputTextAreaHandleKeyDown() {
+  return function (event) {
+    if (event.key === "Enter") {
+      if (!event.shiftKey) {
+        event.preventDefault();
+        sendMessage();
+      }
+    }
+  };
+}
+
+function varsInit() {
+  document.addEventListener('DOMContentLoaded', varsUpdateHeightsAndWidths);
+  window.addEventListener('resize', varsUpdateHeightsAndWidths);
+}
+
+function varsUpdateHeightsAndWidths() {
   let headerHeight = document.querySelector('#header').offsetHeight;
   let userInputHeight = document.querySelector('#userInputPanel').offsetHeight;
   let sendButtonWidth = document.querySelector('#sendButton').offsetWidth;
@@ -89,76 +205,36 @@ function updateWidthsAndHeights() {
   document.documentElement.style.setProperty('--right-side-width', rightSideWidth + 'px');
 }
 
-function toggleTheme() {
-  let body = document.body;
-  let themeToggleIcon = document.getElementById("toggleThemeButton").children[0];
-  if (body.classList.contains("light-theme")) {
-    body.classList.remove("light-theme");
-    themeToggleIcon.classList.remove("fa-toggle-off");
-    themeToggleIcon.classList.add("fa-toggle-on");
-  } else {
-    body.classList.add("light-theme");
-    themeToggleIcon.classList.remove("fa-toggle-on");
-    themeToggleIcon.classList.add("fa-toggle-off");
+function newChat() {
+  chatPanelClear();
+  logoShow();
+  userInputTextAreaFocus();
+  streamingChatCompletionsClear();
+}
+
+function sendMessage() {
+  let inputElement = userInputTextAreaGetElement();
+  let inputValue = inputElement.value;
+
+  let notEmpty = inputValue.trim() !== '';
+  if (notEmpty) {
+    let html = markdownToHtml(inputValue);
+    chatPanelAppendMessage('user', html);
+    userInputTextAreaClear();
+    varsUpdateHeightsAndWidths();
+    streamingChatCompletionsProcessInput(inputValue);
   }
 }
 
-function userInputHandleKeyDown() {
-  return function (event) {
-    if (event.key === "Enter") {
-      if (!event.shiftKey) {
-        event.preventDefault();
-        sendMessage();
-      }
-    }
-  };
-}
+markdownInit();
+toggleThemeButtonInit();
+userInputTextAreaInit();
+varsInit();
+streamingChatCompletionsInit();
 
-function toggleThemeButtonHandleKeyDown() {
-  return function (event) {
-    if (event.code === 'Enter' || event.code === 'Space') {
-      toggleTheme();
-    }
-  };
-}
-
-marked.setOptions({
-  highlight: function(code, lang) {
-    let hl = lang === undefined || lang === ''
-      ? hljs.highlightAuto(code).value
-      : hljs.highlight(lang, code).value;
-    return `<div class="hljs">${hl}</div>`;
-  }
-});
-
-document.getElementById("userInput").addEventListener("keydown", userInputHandleKeyDown());
-document.getElementById("userInput").addEventListener("input", updateUserInputHeight);
-document.addEventListener('DOMContentLoaded', updateWidthsAndHeights);
-window.addEventListener('resize', updateWidthsAndHeights);
-
-document.getElementById("toggleThemeButton").addEventListener("click", toggleTheme);
-document.getElementById("toggleThemeButton").addEventListener('keydown', toggleThemeButtonHandleKeyDown());
+themeToggle();
+userInputTextAreaFocus();
 
 window.sendMessage = sendMessage;
-window.toggleTheme = toggleTheme;
+window.toggleTheme = themeToggle;
 window.newChat = newChat;
-
-toggleTheme();
-document.getElementById("userInput").focus();
-
-const endpoint = process.env.OPENAI_ENDPOINT;
-const azureApiKey = process.env.OPENAI_API_KEY;
-const deploymentName = process.env.AZURE_OPENAI_CHAT_DEPLOYMENT;
-const systemPrompt = "You are a helpful AI assistant."
-
-if (!endpoint) {
-  appendMessage('computer', 'Please set OPENAI_ENDPOINT in .env');
-}
-if (!azureApiKey) {
-  appendMessage('computer', 'Please set OPENAI_API_KEY in .env');
-}
-if (!deploymentName) {
-  appendMessage('computer', 'Please set AZURE_OPENAI_CHAT_DEPLOYMENT in .env');
-}
-
-const streamingChatCompletions = new chatCompletions.StreamingChatCompletionsHelper(systemPrompt, endpoint, azureApiKey, deploymentName)
