da1251472bb0d9cacf588b9f95fd5f1fa077e312
Rob Chambers <robc@thinkman.com>
2023-12-23 15:04:19 -0800

    fix date in other source file

--- src/ai/.x/templates/openai-functions-streaming-js/ChatCompletionsStreaming.js
+++ src/ai/.x/templates/openai-functions-streaming-js/ChatCompletionsStreaming.js
@@ -30,7 +30,7 @@ const getCurrentWeatherSchema = {
 
 function getCurrentDate() {
   const date = new Date();
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
 const getCurrentDateSchema = {
