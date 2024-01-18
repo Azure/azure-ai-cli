a3437506b24ac5aae96d87fc72d67d5794e141f0
Ryan Hurey <rhurey@microsoft.com>
2024-01-18 13:52:02 -0800

    Stashing

---
+++ src/common/details/named_values/tokens/input_chat_history_file_token.cs
@@ -0,0 +1,27 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+namespace Azure.AI.Details.Common.CLI
+{
+    public class InputChatHistoryJsonFileToken
+    {
+        public static NamedValueTokenData Data() => new NamedValueTokenData(_optionName, _parser.FullName, _optionExample, _requiredDisplayName);
+        public static INamedValueTokenParser Parser() => _parser;
+
+        private static _Parser _parser = new();
+        private class _Parser : OutputFileOptionalPrefixNamedValueTokenParser
+        {
+            public _Parser() : base("chat", "history", "1")
+            {
+            }
+
+            public new string FullName => base.FullName;
+        }
+
+        private const string _requiredDisplayName = "chat history input file";
+        private const string _optionName = "--input-chat-history-file";
+        private const string _optionExample = "FILE";
+    }
+}
