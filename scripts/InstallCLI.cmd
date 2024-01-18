12747462fdaa648ba8ee2d0d18026f540a701c82
Panu Koponen <pankopon@microsoft.com>
2024-01-17 17:56:47 -0800

    Fix clean installation

--- scripts/InstallCLI.cmd
+++ scripts/InstallCLI.cmd
@@ -3,10 +3,10 @@ setlocal
 
 set CLI_NUPKG_URL_PREFIX=https://csspeechstorage.blob.core.windows.net/drop/private/ai
 
-REM Azure CLI version
+REM Azure AI CLI version e.g. 1.0.0 or 1.0.0-preview-20231214.1
 if "%~1"=="" (
-  echo Error: Missing parameter for Azure AI CLI version number, e.g. "1.0.0" or "1.0.0-preview-20231214.1" 1>&2
-  exit /b 1
+  echo Error: Azure AI CLI version number is not set. 1>&2
+  goto end
 ) else (
   set CLI_VERSION=%~1
 )
@@ -23,29 +23,31 @@ if exist "%TEMP%"\ (
 REM Windows curl.exe (https://techcommunity.microsoft.com/t5/containers/tar-and-curl-come-to-windows/ba-p/382409)
 where curl.exe >nul
 if %ERRORLEVEL% neq 0 (
-  echo Error: curl.exe not found 1>&2
-  exit /b 2
+  echo Error: curl.exe was not found. 1>&2
+  goto end
 )
 
 REM Dotnet produces an error if there are invalid .nupkgs in the source path.
 del /f /q "%CLI_NUPKG_LOCAL_FOLDER%\Azure.AI.CLI*.nupkg" 2>nul
 
 REM Download Azure AI CLI .nupkg
-curl.exe --output "%CLI_NUPKG_LOCAL_FOLDER%\%CLI_NUPKG_FILE%" --silent --url %CLI_NUPKG_URL_PREFIX%/%CLI_NUPKG_FILE%
+curl.exe --output "%CLI_NUPKG_LOCAL_FOLDER%\%CLI_NUPKG_FILE%" --url %CLI_NUPKG_URL_PREFIX%/%CLI_NUPKG_FILE%
 if %ERRORLEVEL% neq 0 (
-  echo Error while downloading Azure AI CLI .nupkg 1>&2
-  exit /b 3
+  echo Error: Failed to download Azure AI CLI. 1>&2
+  goto end
 )
 
-REM Install Azure CLI
-dotnet tool update --global --add-source "%CLI_NUPKG_LOCAL_FOLDER%" Azure.AI.CLI --version [%CLI_VERSION%]
+REM Install Azure AI CLI
+set DOTNET_NOLOGO=true
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+dotnet tool update --global --add-source "%CLI_NUPKG_LOCAL_FOLDER%" Azure.AI.CLI --verbosity normal --version [%CLI_VERSION%]
 if %ERRORLEVEL% neq 0 (
-  echo Error while installing Azure AI CLI 1>&2
-  exit /b 4
+  echo Error: Failed to install Azure AI CLI. 1>&2
+  goto end
 )
-
-REM Clean up
-del /f /q "%CLI_NUPKG_LOCAL_FOLDER%\%CLI_NUPKG_FILE%" 2>nul
 
 :end
+REM Skip pause with a non-empty second argument
+if "%~2"=="" pause
+
 endlocal
