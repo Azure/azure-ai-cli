7beb0e5011d3e47e72c686a994118b9f9b8ecfa8
Rob Chambers <robc@thinkman.com>
2024-01-27 10:23:26 -0800

    init openai w/ deployments almost works non-interactively

--- src/common/details/commands/init_command.cs
+++ src/common/details/commands/init_command.cs
@@ -407,7 +407,11 @@ namespace Azure.AI.Details.Common.CLI
             var sku = _values.GetOrDefault("init.service.cognitiveservices.resource.sku", Program.CognitiveServiceResourceSku);
             var yes = _values.GetOrDefault("init.service.cognitiveservices.terms.agree", false);
 
-            var resource = await AzCliConsoleGui.PickOrCreateAndConfigCognitiveServicesOpenAiKindResource(interactive, allowSkipDeployments, subscriptionId, regionFilter, groupFilter, resourceFilter, kind, sku, yes);
+            var chatDeploymentFilter = _values.GetOrDefault("init.chat.model.deployment.name", "");
+            var embeddingsDeploymentFilter = _values.GetOrDefault("init.embeddings.model.deployment.name", "");
+            var evaluationsDeploymentFilter = _values.GetOrDefault("init.evaluation.model.deployment.name", "");
+
+            var resource = await AzCliConsoleGui.PickOrCreateAndConfigCognitiveServicesOpenAiKindResource(interactive, allowSkipDeployments, subscriptionId, regionFilter, groupFilter, resourceFilter, kind, sku, yes, chatDeploymentFilter, embeddingsDeploymentFilter, evaluationsDeploymentFilter);
             _values.Reset("service.openai.deployments.picked", "true");
 
             SubscriptionToken.Data().Set(_values, subscriptionId);
