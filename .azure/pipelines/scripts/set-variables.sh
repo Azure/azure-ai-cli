102ea1de57e642aec9a366772b23b0433e935650
Panu Koponen <pankopon@microsoft.com>
2024-01-31 16:48:44 -0800

    update ci files

--- .azure/pipelines/scripts/set-variables.sh
+++ .azure/pipelines/scripts/set-variables.sh
@@ -8,23 +8,32 @@ define_variable () {
 echo "Source branch: $BUILD_SOURCEBRANCH"
 
 # Determine the product version (major.minor.build).
+# NOTE: If the major or minor version is not updated before a new year, the version number becomes ambiguous
+# and it may not be possible to upgrade an old version from the previous year without manual uninstallation.
+# Example:
+# - last build of year N:    1.0.36599
+# - first build of year N+1: 1.0.101   -> cannot update 1.0.36599 with this, must uninstall 1.0.36599 first.
+
 MAJOR_VERSION="1"
 MINOR_VERSION="0"
 BUILD_VERSION="0"
+
 if [ ! -z "$1" ]; then
-    # e.g. "20240120.2" -> BuildMonth 01, BuildDay 20, BuildRunNumber 2
+    # e.g. "20240120.2" -> BuildYear 2024, BuildMonth 01, BuildDay 20, BuildRun 2
+    BuildYear=$(echo "$1" | sed 's/^\([0-9]\{4\}\)[0-9]\{4\}\.[0-9]*$/\1/')
     BuildMonth=$(echo "$1" | sed 's/^[0-9]\{4\}\([0-9]\{2\}\)[0-9]\{2\}\.[0-9]*$/\1/')
     BuildDay=$(echo "$1" | sed 's/^[0-9]\{6\}\([0-9]\{2\}\)\.[0-9]*$/\1/')
-    BuildRunNumber=$(echo "$1" | sed 's/^[0-9]\{8\}\.\([0-9]*$\)/\1/')
-    if [ ! -z "$BuildMonth"     -a $BuildMonth     -ge 1 -a $BuildMonth     -le 12 -a \
-         ! -z "$BuildDay"       -a $BuildDay       -ge 1 -a $BuildDay       -le 31 -a \
-         ! -z "$BuildRunNumber" -a $BuildRunNumber -ge 1 -a $BuildRunNumber -le 99 ]
+    BuildRun=$(echo "$1" | sed 's/^[0-9]\{8\}\.\([0-9]*$\)/\1/')
+
+    if [ ! -z "$BuildMonth" -a $BuildMonth -ge 1 -a $BuildMonth -le 12 -a \
+         ! -z "$BuildDay"   -a $BuildDay   -ge 1 -a $BuildDay   -le 31 -a \
+         ! -z "$BuildRun"   -a $BuildRun   -ge 1 -a $BuildRun   -le 99 ]
     then
         let DayOfYear="($BuildMonth - 1) * 31 + $BuildDay" # estimate using max days/month
-        if [ $BuildRunNumber -lt 10 ]; then
-            BUILD_VERSION="${DayOfYear}0${BuildRunNumber}"
+        if [ $BuildRun -lt 10 ]; then
+            BUILD_VERSION="${DayOfYear}0${BuildRun}"
         else
-            BUILD_VERSION="${DayOfYear}${BuildRunNumber}"
+            BUILD_VERSION="${DayOfYear}${BuildRun}"
         fi
     else
         >&2 echo "Ignored invalid argument: BuildNumber $1"
@@ -33,14 +42,21 @@ fi
 PRODUCT_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${BUILD_VERSION}"
 echo "Product version: $PRODUCT_VERSION"
 
-# If the build was triggered from a tag, use the tag as the version. Otherwise, use the product version.
+if [ ! -z "$2" ]; then
+    BuildId=$2
+    DEV_VERSION="${PRODUCT_VERSION}-dev${BuildYear}.${BuildId}"
+else
+    DEV_VERSION="${PRODUCT_VERSION}-dev${BuildYear}"
+fi
+
+# If the build was triggered from a tag, use the tag as the version. Otherwise, set the version to dev.
 REGEX='^refs\/tags\/v?([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)(-.+)?'
 
 # If tag is a release tag, set up release variables.
 [[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && define_variable "IsRelease" "true" || define_variable "IsRelease" "false"
 
 # Extract version from the tag.
-VERSION=$([[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && echo $(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/') || echo "$PRODUCT_VERSION")
+VERSION=$([[ $BUILD_SOURCEBRANCH =~ $REGEX ]] && echo $(echo $BUILD_SOURCEBRANCH | sed -r 's/'$REGEX'/\1.\2.\3\4/') || echo "$DEV_VERSION")
 
 # Set the AICLIVersion variable in the pipeline.
 define_variable "AICLIVersion" "$VERSION"
--- scripts/BuildInstaller.cmd
+++ scripts/BuildInstaller.cmd
@@ -1,27 +1,34 @@
 @echo off
 setlocal
 
-REM Azure AI CLI version e.g. 1.0.0 or 1.0.0-preview-20231214.1
-if "%~1"=="" (
-  echo Error: Azure AI CLI version number is not set. 1>&2
-  exit /b 1
-)
-
-REM The product version x.y.z shown in the list of installed programs.
-set VERSION=%~1
-REM The AI CLI package (nupkg) version. This cannot be used as a product version.
-set PACKAGE_VERSION=%VERSION%
-
-set PLATFORM=x64
-set INSTALLER=Azure-AI-CLI-Setup-%PACKAGE_VERSION%-%PLATFORM%.exe
-
 REM Dependencies
 set AZURE_CLI_VERSION=2.56.0
 set DOTNET_VERSION=7.0.405
-set DOTNET_URL=https://download.visualstudio.microsoft.com/download/pr/febc46ff-cc68-4bee-83d2-c34786b5ca68/524ef9b25d29dc90efdb0fba0f589779/dotnet-sdk-%DOTNET_VERSION%-win-%PLATFORM%.exe
+set DOTNET_URL=https://download.visualstudio.microsoft.com/download/pr/febc46ff-cc68-4bee-83d2-c34786b5ca68/524ef9b25d29dc90efdb0fba0f589779/dotnet-sdk-%DOTNET_VERSION%-win-%TARGET_PLATFORM%.exe
 set DOTNET_SIZE=229741072
 set DOTNET_SHA1=8d40790ae79bfc6f29f1ab280801e6c019ae5633
 
+REM Azure AI CLI package version (e.g. 1.0.0 or 1.0.0-preview-20231214.1)
+if "%~1"=="" (
+  echo Error: Azure AI CLI package version is not set. 1>&2
+  exit /b 1
+)
+
+REM Azure AI CLI product version x.y.z (e.g. 1.0.0), ref. https://learn.microsoft.com/windows/win32/msi/productversion
+REM This is the version shown in the list of installed programs.
+REM If the package version changes but product version remains the same,
+REM an existing installation cannot be upgraded without uninstalling it first!
+if "%~2"=="" (
+  echo Error: Azure AI CLI product version is not set. 1>&2
+  exit /b 2
+)
+
+set PACKAGE_VERSION=%~1
+set PRODUCT_VERSION=%~2
+set TARGET_PLATFORM=x64
+set INSTALLER_FILE=Azure-AI-CLI-Setup-%PACKAGE_VERSION%-%TARGET_PLATFORM%.exe
+
+REM Check for WiX toolset
 where candle.exe >nul 2>&1
 if %ERRORLEVEL% neq 0 set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\Program Files (x86)\WiX Toolset v3.14\bin
 where candle.exe >nul 2>&1
@@ -30,7 +37,8 @@ if %ERRORLEVEL% neq 0 (
   exit /b 1
 )
 
-candle.exe Azure-AI-CLI.wxs -dversion=%VERSION% -dpackageVersion=%PACKAGE_VERSION% -dplatform=%PLATFORM%
+REM Build AI CLI installer .msi
+candle.exe Azure-AI-CLI.wxs -dproductVersion=%PRODUCT_VERSION% -dpackageVersion=%PACKAGE_VERSION% -dtargetPlatform=%TARGET_PLATFORM%
 if %ERRORLEVEL% neq 0 (
   set EXITCODE=%ERRORLEVEL%
   echo Error from candle.exe [%EXITCODE%] 1>&2
@@ -44,8 +52,9 @@ if %ERRORLEVEL% neq 0 (
   exit /b %EXITCODE%
 )
 
+REM Build installation bundle .exe
 candle.exe Azure-AI-CLI-Bundle.wxs -ext WixBalExtension -ext WixUtilExtension ^
-  -dversion=%VERSION% -dplatform=%PLATFORM% -dazureCliVersion=%AZURE_CLI_VERSION% ^
+  -dproductVersion=%PRODUCT_VERSION% -dtargetPlatform=%TARGET_PLATFORM% -dazureCliVersion=%AZURE_CLI_VERSION% ^
   -ddotNetVersion=%DOTNET_VERSION% -ddotNetUrl=%DOTNET_URL% -ddotNetSize=%DOTNET_SIZE% -ddotNetSha1=%DOTNET_SHA1%
 if %ERRORLEVEL% neq 0 (
   set EXITCODE=%ERRORLEVEL%
@@ -53,7 +62,7 @@ if %ERRORLEVEL% neq 0 (
   exit /b %EXITCODE%
 )
 
-light.exe Azure-AI-CLI-Bundle.wixobj -ext WixBalExtension -ext WixUtilExtension -o %INSTALLER%
+light.exe Azure-AI-CLI-Bundle.wixobj -cultures:en-us -loc Azure-AI-CLI-Bundle.wxl -ext WixBalExtension -ext WixUtilExtension -out %INSTALLER_FILE%
 if %ERRORLEVEL% neq 0 (
   set EXITCODE=%ERRORLEVEL%
   echo Error from light.exe [%EXITCODE%] 1>&2
@@ -61,5 +70,5 @@ if %ERRORLEVEL% neq 0 (
 )
 
 :end
-echo Run %INSTALLER% to install.
+echo Run %INSTALLER_FILE% to install.
 endlocal
