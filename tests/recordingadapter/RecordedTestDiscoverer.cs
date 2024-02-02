2aa2ef90ef92a681611143ba22c2fe9d4c09df1a
Ryan Hurey <rhurey@microsoft.com>
2024-02-01 22:43:40 -0800

    Initial add

---
+++ tests/recordingadapter/RecordedTestDiscoverer.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.Details.Common.CLI.TestFramework;
+
+namespace Azure.AI.Details.Common.CLI.RecordedTestAdapter
+{
+    [FileExtension(YamlTestFramework.YamlFileExtension)]
+    [FileExtension(RecordedTestAdapter.DllFileExtension)]
+    [DefaultExecutorUri(RecordedTestAdapter.Executor)]
+    public class RecordingDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            try
+            {
+                Logger.Log(logger);
+                Logger.Log($"RecordingDiscoverer.DiscoverTests(): ENTER");
+                Logger.Log($"RecordingDiscoverer.DiscoverTests(): count={sources.Count()}");
+                foreach (var test in RecordedTestAdapter.GetTestsFromFiles(sources))
+                {
+                    test.ExecutorUri = new Uri(RecordedTestAdapter.Executor);
+                    if (test.Traits.Any((Trait trait) => trait.Name == "tag" && trait.Value == "recordable" || trait.Name == "recordable" && trait.Value == "true"))
+                    {
+                        discoverySink.SendTestCase(test);
+                    }
+
+                }
+                Logger.Log($"RecordingDiscoverer.DiscoverTests(): EXIT");
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"EXCEPTION: {ex.Message}\nSTACK: {ex.StackTrace}");
+                throw;
+            }
+        }
+    }
+}
---
+++ tests/recordingadapter/TestProxyClient.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace YamlTestAdapter
+{
+    public class TestProxyClient
+    {
+        private const string _proxy = "http://localhost:5004";
+
+        private static readonly HttpClient _httpClient = new HttpClient(new HttpClientHandler()
+        {
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+        });
+
+        public enum SanatizeLocation
+        {
+            Header,
+            Body,
+            Uri
+        }
+
+        /*
+   private static async Task Record()
+   {
+       var recordingId = await StartRecording();
+       await SendRequest(recordingId, "record");
+       await Task.Delay(TimeSpan.FromSeconds(2));
+       await SendRequest(recordingId, "record");
+       await StopRecording(recordingId);
+   }
+
+   private static async Task Playback()
+   {
+       var recordingId = await StartPlayback();
+       await SendRequest(recordingId, "playback");
+       await SendRequest(recordingId, "playback");
+       await StopPlayback(recordingId);
+   }
+*/
+
+        public static Process InvokeProxy()
+        {
+            var startInfo = new ProcessStartInfo("test-proxy")
+            {
+                UseShellExecute = false,
+            };
+
+            var process = Process.Start(startInfo);
+
+            return process;
+        }
+
+        public static void StopProxy(Process process)
+        {
+            process.Kill();
+        }
+
+        public static async Task<string> StartPlayback(string recordingFile)
+        {
+            Console.WriteLine("StartPlayback");
+
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + "/playback/start");
+
+            var json = "{\"x-recording-file\":\"" + recordingFile + "\"}";
+            var content = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
+            message.Content = content;
+
+            var response = await _httpClient.SendAsync(message);
+            var recordingId = response.Headers.GetValues("x-recording-id").Single();
+            Console.WriteLine($"  x-recording-id: {recordingId}");
+            Console.WriteLine();
+
+            return recordingId;
+        }
+
+        public static async Task StopPlayback(string recordingId)
+        {
+            Console.WriteLine("StopPlayback");
+            Console.WriteLine();
+
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + "/playback/stop");
+            message.Headers.Add("x-recording-id", recordingId);
+
+            await _httpClient.SendAsync(message);
+        }
+
+        public static async Task<string> StartRecording(string recordingFile)
+        {
+            Console.WriteLine("StartRecording");
+
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + "/record/start");
+
+            var json = "{\"x-recording-file\":\"" + recordingFile + "\"}";
+            var content = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
+            message.Content = content;
+
+            var response = await _httpClient.SendAsync(message);
+            var recordingId = response.Headers.GetValues("x-recording-id").Single();
+            Console.WriteLine($"  x-recording-id: {recordingId}");
+            Console.WriteLine();
+
+            return recordingId;
+        }
+
+        public static async Task StopRecording(string recordingId)
+        {
+            Console.WriteLine("StopRecording");
+            Console.WriteLine();
+
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + "/record/stop");
+            message.Headers.Add("x-recording-id", recordingId);
+            message.Headers.Add("x-recording-save", bool.TrueString);
+
+            await _httpClient.SendAsync(message);
+        }
+
+        public static Task AddUriSinatizer(string regexToMatch, string replaceValue) => AddSanitizer("UriRegexSanitizer", $"{{\"value\": \"{replaceValue}\",\"regex\": \"{regexToMatch}\"}}");
+        
+        public static Task AddHeaderSanitizer(string key, string value) => AddSanitizer("HeaderRegexSanitizer", $"{{\"value\": \"{value}\",\"key\": \"{key}\"}}");
+
+        private static async Task AddSanitizer(string headerName, string json)
+        {
+            var url = "/admin/addsanitizer";
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + url);
+            message.Headers.Add("x-abstraction-identifier", headerName);
+            message.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await _httpClient.SendAsync(message);
+            if(result.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Failed to add sanitizer");
+            }
+        }
+
+        public static async Task ClearSanatizers()
+        {
+            var url = "/admin/reset";
+            var message = new HttpRequestMessage(HttpMethod.Post, _proxy + url);
+            var result = await _httpClient.SendAsync(message);
+
+            if(result.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Failed to clear sanitizers");
+            }
+        }
+        /*
+            public static async Task SendRequest(string recordingId, string mode)
+            {
+                Console.WriteLine("Request");
+
+                var message = new HttpRequestMessage(HttpMethod.Get, _proxy);
+                message.Headers.Add("x-recording-id", recordingId);
+                message.Headers.Add("x-recording-mode", mode);
+                message.Headers.Add("x-recording-upstream-base-uri", _url);
+
+                var response = await _httpClient.SendAsync(message);
+                var body = (await response.Content.ReadAsStringAsync());
+
+                Console.WriteLine("Headers:");
+                Console.WriteLine($"  Date: {response.Headers.Date.Value.LocalDateTime}");
+                Console.WriteLine($"Body: {(body.Replace("\r", string.Empty).Replace("\n", string.Empty).Substring(0, 80))}");
+                Console.WriteLine();
+            }
+        }
+        */
+    }
+
+}
