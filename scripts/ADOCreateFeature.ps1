488b250864af0f565342a95da85a8b84c81993bc
Chris Schraer <chris_schraer@noreply.com>
2024-02-13 09:42:22 -0800

    removing prints from create

--- scripts/ADOCreateFeature.ps1
+++ scripts/ADOCreateFeature.ps1
@@ -20,7 +20,6 @@ param (
 
 # Define the URL for the REST API call
 $url = "https://dev.azure.com/$organization/$project/_apis/wit/workitems/`$$($workItemType)?api-version=7.1"
-Write-Host "URL: $url"
 
 $body = @(
    [ordered] @{  op = 'add';  path = '/fields/System.Title';  value = "$title"  }
@@ -31,7 +30,6 @@ $body = @(
 
 # Convert the body to JSON
 $bodyJson = ConvertTo-Json -InputObject $body
-Write-Host "Body: $bodyJson"
 
 $B64Pat = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("`:$pat"))
 
@@ -43,8 +41,6 @@ $headers = @{
 
 # Make the REST API call to create the work item
 $response = Invoke-RestMethod -Uri $url -Method Post -Body $bodyJson -Headers $headers
-Write-Host "Work item created with response: $response"
-Write-Host "Work item created with ID: $($response.id)"
 
 # Output the ID of the new work item
 $($response.id)
