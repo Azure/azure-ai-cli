4cbab469b082ec61bd294eeed7632f606c350fd0
Rob Chambers <robc@thinkman.com>
2024-02-13 20:09:22 -0800

    comment out stuff that doesn't work

--- src/ai/commands/search_command.cs
+++ src/ai/commands/search_command.cs
@@ -161,11 +161,12 @@ namespace Azure.AI.Details.Common.CLI
 
         private string DoIndexUpdateWithGenAi(string subscription, string groupName, string projectName, string indexName, string embeddingModelDeployment, string embeddingModelName, string dataFiles, string externalSourceUrl)
         {
-            // work around issue with Py GenAI SDK needing this var to be set; do not set any additional values... See Hanchi Wang for more info.
-            var env = ConfigEnvironmentHelpers.GetEnvironment(_values);
-            env = new Dictionary<string, string>(env.Where(x => x.Key == "AZURE_OPENAI_KEY"));
+            // this didn't work... commenting out until we have a more definitive solution
+            // // work around issue with Py GenAI SDK needing this var to be set; do not set any additional values... See Hanchi Wang for more info.
+            // var env = ConfigEnvironmentHelpers.GetEnvironment(_values);
+            // env = new Dictionary<string, string>(env.Where(x => x.Key == "AZURE_OPENAI_KEY"));
             
-            return PythonSDKWrapper.UpdateMLIndex(_values, subscription, groupName, projectName, indexName, embeddingModelDeployment, embeddingModelName, dataFiles, externalSourceUrl, env);
+            return PythonSDKWrapper.UpdateMLIndex(_values, subscription, groupName, projectName, indexName, embeddingModelDeployment, embeddingModelName, dataFiles, externalSourceUrl, null);
         }
 
         private async Task<string> DoIndexUpdateWithAISearch(string aiServicesApiKey, string searchEndpoint, string searchApiKey, string embeddingsEndpoint, string embeddingsDeployment, string embeddingsApiKey, string searchIndexName, string dataSourceConnectionName, string blobContainer, string pattern, string skillsetName, string indexerName, string idFieldName, string contentFieldName, string vectorFieldName)
