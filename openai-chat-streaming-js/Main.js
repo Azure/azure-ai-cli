172fc955ff85f8cb7c82c69da9cdbe8ace8d0a53
Rob Chambers <robc@thinkman.com>
2024-03-30 16:26:42 -0700

    streaming one works with both openai and with azure openai now

--- openai-chat-streaming-js/Main.js
+++ openai-chat-streaming-js/Main.js
@@ -8,18 +8,32 @@ const rl = readline.createInterface({
 
 async function main() {
 
-  const openAIKey = "YOUR-KEY-HERE";
-  const openAIOrganization = null;
-  const openAIModel = "gpt-4-turbo-preview";
-  const openAISystemPrompt = "You are a helpful AI assistant.";
+  const openAISystemPrompt = process.env["AZURE_OPENAI_SYSTEM_PROMPT"] || "You are a helpful AI assistant.";
 
-  const chat = new OpenAIChatCompletionsStreamingClass(openAIKey, openAIOrganization, openAIModel, openAISystemPrompt);
+  // OpenAI API
+  const openAIKey = process.env["OPENAI_API_KEY"] || "YOUR-KEY-HERE";
+  const openAIModelName = process.env["OPENAI_MODEL_NAME"] || "gpt-4-turbo-preview";
+  const openAIOrganization = process.env["OPENAI_ORG_ID"] || null;
 
+  // Azure OpenAI API
+  const azureOpenAIAPIVersion = process.env["AZURE_OPENAI_API_VERSION"] || "2024-03-01-preview";
+  const azureOpenAIEndpoint = process.env["AZURE_OPENAI_ENDPOINT"] || "<insert your Azure OpenAI endpoint here>";
+  const azureOpenAIKey = process.env["AZURE_OPENAI_API_KEY"] || "<insert your Azure OpenAI API key here>";
+  const azureOpenAIChatDeploymentName = process.env["AZURE_OPENAI_CHAT_DEPLOYMENT"] || "<insert your Azure OpenAI chat deployment name here>";
+
+  // Create the right one based on what is available
+  const chat = !azureOpenAIEndpoint?.startsWith("https://")
+    ? OpenAIChatCompletionsStreamingClass.createUsingOpenAI(openAIKey, openAIModelName, openAISystemPrompt, openAIOrganization)
+    : OpenAIChatCompletionsStreamingClass.createUsingAzure(azureOpenAIAPIVersion, azureOpenAIEndpoint, azureOpenAIKey, azureOpenAIChatDeploymentName, openAISystemPrompt);
+
+  // Start the conversation, and loop until the user types 'exit'
   while (true) {
 
+    // Get user input
     const input = await new Promise(resolve => rl.question('User: ', resolve));
     if (input === 'exit' || input === '') break;
 
+    // Get the Assistant's response
     process.stdout.write('\nAssistant: ');
     await chat.getResponse(input, (content) => {
       process.stdout.write(content);
--- openai-chat-streaming-js/OpenAIChatCompletionsStreamingClass.js
+++ openai-chat-streaming-js/OpenAIChatCompletionsStreamingClass.js
@@ -1,29 +1,52 @@
 const { OpenAI } = require('openai');
 
 class OpenAIChatCompletionsStreamingClass {
-  constructor(openAIKey, openAIOrganization, openAIModelName, openAISystemPrompt) {
-    this.openAISystemPrompt = openAISystemPrompt;
-    this.openAIModelName = openAIModelName;
-    this.openai = new OpenAI({
-      apiKey: openAIKey,
-      organization: openAIOrganization,
-    });
-    
+
+  // Create the class using the Azure OpenAI API, which requires a different setup, baseURL, api-key headers, and query parameters
+  static createUsingAzure(azureOpenAIAPIVersion, azureOpenAIEndpoint, azureOpenAIKey, azureOpenAIDeploymentName, systemPrompt) {
+    console.log("Using Azure OpenAI API...");
+    return new OpenAIChatCompletionsStreamingClass(azureOpenAIDeploymentName, systemPrompt,
+      new OpenAI({
+        apiKey: azureOpenAIKey,
+        baseURL: `${azureOpenAIEndpoint.replace(/\/+$/, '')}/openai/deployments/${azureOpenAIDeploymentName}`,
+        defaultQuery: { 'api-version': azureOpenAIAPIVersion },
+        defaultHeaders: { 'api-key': azureOpenAIKey },
+      }));
+  }
+
+  // Create the class using the OpenAI API and an optional organization
+  static createUsingOpenAI(openAIKey, openAIModelName, systemPrompt, openAIOrganization = null) {
+    console.log("Using OpenAI API...");
+    return new OpenAIChatCompletionsStreamingClass(openAIModelName, systemPrompt,
+      new OpenAI({
+        apiKey: openAIKey,
+        organization: openAIOrganization,
+      }));
+  }
+
+  // Constructor
+  constructor(openAIModelOrDeploymentName, systemPrompt, openai) {
+    this.systemPrompt = systemPrompt;
+    this.openAIModelOrDeploymentName = openAIModelOrDeploymentName;
+    this.openai = openai;
+  
     this.clearConversation();
   }
 
+  // Clear the conversation
   clearConversation() {
     this.messages = [
-      { role: 'system', content: this.openAISystemPrompt }
+      { role: 'system', content: this.systemPrompt }
     ];
   }
 
+  // Get the response from the Assistant
   async getResponse(userInput, callback) {
     this.messages.push({ role: 'user', content: userInput });
 
     let response = '';
     const events = await this.openai.chat.completions.create({
-      model: this.openAIModelName,
+      model: this.openAIModelOrDeploymentName,
       messages: this.messages,
       stream: true
     });
